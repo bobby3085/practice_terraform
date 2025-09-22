4bb66637740cf52eb12acad408ad54136df55698
bobby3085 <bobby3085@gmail.com>
2025-09-22 09:57:27 +0000

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,17 @@
 #use a data source to look up the latest Amazon Linux AMI
 
-data "aws_ami" "amazon_ubuntu" {
+data "aws_ami" "ubuntu" {
   most_recent = true
-  owners - ["amazon"]
   
+  owners = ["099720109477"]
+
   filter {
     name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name = "virtualization-type"
     values = ["hvm"]
   }
 }
@@ -14,14 +20,14 @@ data "aws_ami" "amazon_ubuntu" {
 
 # this is the important part which defines what we need
 
-resource "aws_instance" "ec2_by_terraform" {  	# "ec2_by_terraform" is the name we are giving to instance,
+resource "aws_instance" "web_server" {  	# "ec2_by_terraform" is the name we are giving to instance,
 						#"aws_instance" means we the resource we need from aws is an instance, we can replace with s3 etc
-  ami = data.aws_ami.amazon_ubuntu.id 			# we can get ami-xxx number and type t2.micro or t3.micro etc from aws
-  isntance_type = var.instance_type
-  key_name = var.key-name
+  ami = data.aws_ami.ubuntu.id 			# we can get ami-xxx number and type t2.micro or t3.micro etc from aws
+  instance_type = var.instance_type
+  key_name = var.key_name
 
   tags = {
-    Name = "${var.instance_name}-Instance"		# tags block allows us to apply key-value tags to our EC2 instance for organization and identification
+    Name = "${var.instance_name}-Instance"	# tags block allows us to apply key-value tags to our EC2 instance for organization and identification
     Environment = "Development"
   }
 }
