325b5e7d6a7e57ea82b33eac0f5f2c0f3c81baaa
bobby3085 <bobby3085@gmail.com>
2025-09-23 08:59:36 +0530

    Update variables.tf
    added comments

--- variables.tf
+++ variables.tf
@@ -1,18 +1,19 @@
 # creating variables 
 
-
+# creating variable for instance_name
 variable "instance_name" {
   description = "The name for the EC2 instance. "
   type = string
   default = " terrafrom-exp "
 }
 
+#creating variable instance_type
 variable "instance_type" {
   description = "The EC2 instance type"
   type = string
   default = "t2.micro"
 }
-
+# creating variable for count : no_of_instances
 variable "no_of_instances" {
   description = "Enter how many instances needed:"
   type = number
